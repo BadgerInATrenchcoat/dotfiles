f29633cead63a398cd5a7a49e1fa545660f365f6
pika-colony <169167220+pika-colony@users.noreply.github.com>
2024-06-27 15:20:21 -0400

    Change doom theme and spellchecker

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -32,7 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-palenight)
+;; (setq doom-theme 'doom-challenger-deep)
+(setq doom-theme 'kaolin-aurora)
 
 (setq doom-font (font-spec :family "JetBrains Mono" :size 12 :weight 'medium)
       doom-variable-pitch-font (font-spec :family "JetBrains Mono" :size 13))
--- .config/doom/init.el
+++ .config/doom/init.el
@@ -82,7 +82,7 @@
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +hunspell) ; tasing you for misspelling mispelling
+       (spell +everything) ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
        :tools
